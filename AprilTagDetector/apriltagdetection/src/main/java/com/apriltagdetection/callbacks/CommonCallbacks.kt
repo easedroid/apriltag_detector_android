18aa4b455a27701813974647ac7094ef295dbee8
Amit Kar <amit.dgp.ak@gmail.com>
2020-05-21 19:53:17 +0530

    Remove duplicated directory

--- AprilTagDetector/apriltagdetection/src/main/java/com/apriltagdetection/callbacks/CommonCallbacks.kt
+++
@@ -1,5 +0,0 @@
-package com.apriltagdetection.callbacks
-
-interface CommonCallbacks {
-    fun startDetection(byteArray: ByteArray, width: Int, height: Int)
-}
--- AprilTagDetector/apriltagdetection/src/main/java/com/apriltagdetection/callbacks/DrawingCallback.kt
+++
@@ -1,8 +0,0 @@
-package com.apriltagdetection.callbacks
-
-import com.apriltagdetection.config.model.ApriltagDetection
-
-interface DrawingCallback {
-    fun onMarkerDraw(marker: ApriltagDetection)
-    fun clearMarkerView()
-}
